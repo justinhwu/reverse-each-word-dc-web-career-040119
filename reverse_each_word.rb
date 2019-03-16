8740722f127c0b35a10fd541eb5e3f60733c0076
Justin Wu <justinhwu95@gmail.com>
2019-03-16 16:53:42 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,14 @@
+require 'pry'
+def reverse_each_word(sentence)
+  array = sentence.reverse.split()
+  reversed = []
+  array.reverse.each do |x| 
+    reversed.push(x)
+    #/binding.pry
+  end
+  reversed.join(" ")
+  #/binding.pry
+  
+  array.reverse.collect do |x|
+  
+end
